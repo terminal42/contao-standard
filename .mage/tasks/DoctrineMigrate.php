bd17c17ab34a616d56f300fa50496366e2686ced
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-06-30 09:51:03 +0200

    Fix doctrine migrations if there are no migration tasks

---
+++ .mage/tasks/DoctrineMigrate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Task;
+
+use Mage\Task\BuiltIn\Symfony2\SymfonyAbstractTask;
+
+class DoctrineMigrateTask extends SymfonyAbstractTask
+{
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'Symfony v2 - Migrate doctrine entities';
+    }
+
+    /**
+     * Migrates Doctrine entities
+     *
+     * @see \Mage\Task\AbstractTask::run()
+     */
+    public function run()
+    {
+        $env = $this->getParameter('env', 'dev');
+
+        $command = $this->getAppPath() . ' doctrine:migrations:migrate -n --env=' . $env . ' --allow-no-migration';
+
+        return $this->runCommand($command);
+    }
+}

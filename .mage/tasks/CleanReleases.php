2283a47de0dd1dc36316e3c4652b5c17a3577a75
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-11-23 20:13:19 +0100

    CleanReleases task now supports multiple hosts

--- .mage/tasks/CleanReleases.php
+++ .mage/tasks/CleanReleases.php
@@ -2,6 +2,7 @@
 
 namespace Task;
 
+use Mage\Console;
 use Mage\Task\AbstractTask;
 
 class CleanReleases extends AbstractTask
@@ -23,13 +24,50 @@ class CleanReleases extends AbstractTask
             return false;
         }
 
-        $releasesDirectory = $this->getConfig()->release('directory', 'releases');
-        $releasesDirectory = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $releasesDirectory;
-
         $maxReleases = $this->getParameter('max', 5);
         if (!$maxReleases) {
             return false;
         }
+
+        $hosts = $this->getConfig()->getHosts();
+
+        if (0 === count($hosts)) {
+            Console::output('<light_purple>Warning!</light_purple> <bold>No hosts defined, skipping deployment tasks.</bold>', 1, 3);
+
+            return false;
+        }
+
+        Console::output('', 1, 1);
+
+        foreach ($hosts as $hostKey => $host) {
+
+            // Check if Host has specific configuration
+            $hostConfig = null;
+            if (is_array($host)) {
+                $hostConfig = $host;
+                $host       = $hostKey;
+            }
+
+            // Set Host and Host Specific Config
+            $this->getConfig()->setHost($host);
+            $this->getConfig()->setHostConfig($hostConfig);
+
+            if (false === $this->cleanHost($maxReleases)) {
+                return false;
+            }
+        }
+
+        $this->getConfig()->setHostConfig(null);
+
+        Console::output('all ', 3, 0);
+
+        return true;
+    }
+
+    private function cleanHost($maxReleases)
+    {
+        $releasesDirectory = $this->getConfig()->release('directory', 'releases');
+        $releasesDirectory = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $releasesDirectory;
 
         $releasesList = '';
         $countReleasesFetch = $this->runCommandRemote('ls -1 ' . $releasesDirectory, $releasesList);
@@ -45,14 +83,27 @@ class CleanReleases extends AbstractTask
             sort($releasesToDelete);
             $releasesToDeleteCount = count($releasesToDelete) - $maxReleases;
             $releasesToDelete = array_slice($releasesToDelete, 0, $releasesToDeleteCount + 1);
+            $first = true;
+
+            Console::output(sprintf('from <bold>%s</bold>: ', $this->getConfig()->getHost()), 3, 0);
 
             foreach ($releasesToDelete as $releaseIdToDelete) {
+                if (!$first) {
+                    Console::output(', ', 0, 0);
+                }
+
+                Console::output('<purple>'.$releaseIdToDelete.'</purple>', 0, 0);
+
                 $directoryToDelete = $releasesDirectory . '/' . $releaseIdToDelete;
                 if ('/' !== $directoryToDelete) {
                     $command = 'rm -rf ' . $directoryToDelete;
                     $this->runCommandRemote($command);
                 }
+
+                $first = false;
             }
+
+            Console::output('', 0, 1);
         }
 
         return true;

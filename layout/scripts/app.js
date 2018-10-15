697a5e6fc5fc48568854b7869205d098becc6f13
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-10-15 08:44:02 +0200

    automatically add all images to the manifest.json

--- layout/scripts/app.js
+++ layout/scripts/app.js
@@ -1,2 +1,6 @@
 
-require('../styles/app.scss');
+import '../styles/app.scss';
+
+// automatically add all images to the manifest.json
+const imagesCtx = require.context('../images', false, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesCtx.keys().forEach(imagesCtx);
